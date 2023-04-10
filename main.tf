72ac4f14c4b1a80e5fdd19f3ee2e3b631b13a14e
nikhilt <nikhil.t1226@gmail.com>
2023-04-10 01:43:19 +0100

    \ |"?

--- main.tf
+++ main.tf
@@ -17,3 +17,16 @@ resource "aws_subnet" "public_subnets" {
   availability_zone = each.value["availability_zone"]
 
 }
+
+# Private subnets
+
+resource "aws_subnet" "private_subnets" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" }
+  )
+
+  for_each          = var.private_subnets
+  cidr_block        = each.value["cidr_block"]
+  availability_zone = each.value["availability_zone"]
+
+}
