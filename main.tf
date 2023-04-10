3c609fb9b4bc615c0f718da6e5f6858e1101bbb5
nikhilt <nikhil.t1226@gmail.com>
2023-04-10 02:47:56 +0100

    \ |"?

--- main.tf
+++ main.tf
@@ -54,3 +54,12 @@ resource "aws_route_table" "private-route-table" {
     for_each     = var.private_subnets
     tags         = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" })
   }
+
+
+resource "aws_route_table_association" "private-association" {
+    for_each          = var.private_subnets
+    subnet_id      = lookup(lookup(aws_subnet.private_subnets, each.value["name"], null), "id", null)
+    #    subnet_id      = aws_subnet.private_subnets[each.value["name"]].id
+    route_table_id = aws_route_table.private-route-table[each.value["name"]].id
+
+}
