8550dd622a55ef7f71b776abe92c90598303e30a
nikhilt <nikhil.t1226@gmail.com>
2023-04-10 01:20:38 +0100

    \ |"?

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ resource "aws_vpc" "main" {
   tags = merge(var.tags, { Name = "${var.env}-vpc" }
   )
   }
+
+
+# Public subnets
+
+resource "aws_subnet" "public_subnets" {
+  for_each = var.public_subnets
+  vpc_id = aws_vpc.main.id
+  cidr_block = each.value["availability_zone"]
+  tags = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" }
+  )
+}
+}
