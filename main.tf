1c158f5a6271cecf69021b092aea16bc05510957
nikhilt <nikhil.t1226@gmail.com>
2023-04-10 02:58:22 +0100

    \ |"?

--- main.tf
+++ main.tf
@@ -15,10 +15,22 @@ resource "aws_subnet" "public_subnets" {
   }
 
 
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+
+    tags         = merge(var.tags, { Name = "${var.env}-igw"})
+}
 
 # Public Route Table
 resource "aws_route_table" "public-route-table" {
     vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+
 
     for_each     = var.public_subnets
     tags         = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" })
@@ -27,9 +39,9 @@ resource "aws_route_table" "public-route-table" {
 
 resource "aws_route_table_association" "public-association" {
     for_each          = var.public_subnets
-    subnet_id      = lookup(lookup(aws_subnet.public_subnets, each.value["name"], null), "id", null)
-#    subnet_id      = aws_subnet.public_subnets[each.value["name"]].id
-    route_table_id = aws_route_table.public-route-table[each.value["name"]].id
+    subnet_id         = lookup(lookup(aws_subnet.public_subnets, each.value["name"], null), "id", null)
+#    subnet_id        = aws_subnet.public_subnets[each.value["name"]].id
+    route_table_id    = aws_route_table.public-route-table[each.value["name"]].id
 
 }
 
@@ -58,8 +70,9 @@ resource "aws_route_table" "private-route-table" {
 
 resource "aws_route_table_association" "private-association" {
     for_each          = var.private_subnets
-    subnet_id      = lookup(lookup(aws_subnet.private_subnets, each.value["name"], null), "id", null)
-    #    subnet_id      = aws_subnet.private_subnets[each.value["name"]].id
-    route_table_id = aws_route_table.private-route-table[each.value["name"]].id
+    subnet_id         = lookup(lookup(aws_subnet.private_subnets, each.value["name"], null), "id", null)
+    #    subnet_id    = aws_subnet.private_subnets[each.value["name"]].id
+    route_table_id    = aws_route_table.private-route-table[each.value["name"]].id
 
 }
+
