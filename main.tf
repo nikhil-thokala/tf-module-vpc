cc9cead3194e9a732f58826b68519ff4262510bb
nikhilt <nikhil.t1226@gmail.com>
2023-04-10 02:15:39 +0100

    \ |"?

--- main.tf
+++ main.tf
@@ -1,32 +1,49 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  tags = merge(var.tags, { Name = "${var.env}-vpc" }
-  )
-  }
+    cidr_block = var.vpc_cidr
+    tags = merge(var.tags, { Name = "${var.env}-vpc" })
+    }
 
 
 # Public subnets
-
 resource "aws_subnet" "public_subnets" {
-  vpc_id = aws_vpc.main.id
-  tags = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" }
-  )
+    vpc_id = aws_vpc.main.id
 
-  for_each          = var.public_subnets
-  cidr_block        = each.value["cidr_block"]
-  availability_zone = each.value["availability_zone"]
+    for_each          = var.public_subnets
+    cidr_block        = each.value["cidr_block"]
+    availability_zone = each.value["availability_zone"]
+    tags              = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" })
 
-}
+  }
+
+
+
+# Public Route Table
+resource "aws_route_table" "public-route-table" {
+    vpc_id = aws_vpc.main.id
+
+    for_each     = var.public_subnets
+    tags         = merge(var.tags, { Name = "${var.env}-vpc" })
+  }
+
+
 
 # Private subnets
-
 resource "aws_subnet" "private_subnets" {
-  vpc_id = aws_vpc.main.id
-  tags = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" }
-  )
+    vpc_id = aws_vpc.main.id
 
-  for_each          = var.private_subnets
-  cidr_block        = each.value["cidr_block"]
-  availability_zone = each.value["availability_zone"]
+    for_each          = var.private_subnets
+    cidr_block        = each.value["cidr_block"]
+    availability_zone = each.value["availability_zone"]
+    tags              = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" })
 
-}
+  }
+
+
+
+# Private Route Table
+resource "aws_route_table" "private-route-table" {
+    vpc_id = aws_vpc.main.id
+
+    for_each     = var.private_subnets
+    tags         = merge(var.tags, { Name = "${var.env}-vpc" })
+  }
