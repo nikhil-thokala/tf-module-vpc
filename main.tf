4706918f63afa3dc77b1d0b6f39792ffc174d357
nikhilt <nikhil.t1226@gmail.com>
2023-04-10 03:53:32 +0100

    \ |"?

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "aws_route_table" "private-route-table" {
     for_each     = var.private_subnets
     route {
         cidr_block     = "0.0.0.0/0"
-        nat_gateway_id = aws_nat_gateway.nat-gateways[each.value["availability_zone"]].id
+        nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
     }
 
     tags         = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" })
