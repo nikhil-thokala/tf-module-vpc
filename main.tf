ebcd00e441f296bba321806672dee3b8dad0dd26
nikhilt <nikhil.t1226@gmail.com>
2023-04-11 21:55:26 +0100

    \ |"?

--- main.tf
+++ main.tf
@@ -75,50 +75,49 @@ resource "aws_route_table_association" "public-association" {
 }
 
 
-
-# Private subnets
+# Private Subnets
 resource "aws_subnet" "private_subnets" {
     vpc_id = aws_vpc.main.id
 
     for_each          = var.private_subnets
     cidr_block        = each.value["cidr_block"]
     availability_zone = each.value["availability_zone"]
-    tags              = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" })
-  }
+    tags = merge(
+        var.tags,
+        { Name = "${var.env}-${each.value["name"]}" }
+    )
 
+}
 
-# Private Route Table
+# Private Route table
 resource "aws_route_table" "private-route-table" {
     vpc_id = aws_vpc.main.id
 
-    for_each     = var.private_subnets
+    for_each = var.private_subnets
     route {
         cidr_block     = "0.0.0.0/0"
         nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
     }
-
     route {
-        cidr_block = data.aws_vpc.default_vpc.cidr_block
+        cidr_block                = data.aws_vpc.default_vpc.cidr_block
         vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-     }
-
-    tags         = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" })
-  }
-
-
-resource "aws_route_table_association" "private-association" {
-    for_each          = var.private_subnets
-    subnet_id         = lookup(lookup(aws_subnet.private_subnets, each.value["name"], null), "id", null)
-    #  subnet_id    = aws_subnet.private_subnets[each.value["name"]].id
-    route_table_id    = aws_route_table.private-route-table[each.value["name"]].id
-
+    }
+    tags = merge(
+        var.tags,
+        { Name = "${var.env}-${each.value["name"]}" }
+    )
 }
 
-# Route to the default VPC peering to work
+resource "aws_route_table_association" "private-association" {
+    for_each       = var.private_subnets
+    subnet_id      = lookup(lookup(aws_subnet.private_subnets, each.value["name"], null), "id", null)
+    route_table_id = aws_route_table.private-route-table[each.value["name"]].id
+}
+
+## Route to the default VPC for peering to work.
 
 resource "aws_route" "route" {
-            route_table_id              = var.default_route_table
-            destination_cidr_block     = var.vpc_cidr
-            vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
-        }
-
+    route_table_id            = var.default_route_table
+    destination_cidr_block    = var.vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
